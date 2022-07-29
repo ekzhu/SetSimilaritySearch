8edd285654f3dce520c954a830c91c097b6b46f5
Eric Zhu <ekzhu@users.noreply.github.com>
2022-07-29 23:13:42 +0000

    use set size as argument to position filter and similarity functions to allow using real query set size. Fix #13.

--- SetSimilaritySearch/all_pairs.py
+++ SetSimilaritySearch/all_pairs.py
@@ -2,12 +2,16 @@ import logging
 from collections import defaultdict
 import numpy as np
 
-from SetSimilaritySearch.utils import _frequency_order_transform, \
-        _similarity_funcs, _overlap_threshold_funcs, _position_filter_funcs, \
-        _symmetric_similarity_funcs
+from SetSimilaritySearch.utils import (
+    _frequency_order_transform,
+    _similarity_funcs,
+    _overlap_threshold_funcs,
+    _position_filter_funcs,
+    _symmetric_similarity_funcs,
+)
 
-def all_pairs(sets, similarity_func_name="jaccard",
-        similarity_threshold=0.5):
+
+def all_pairs(sets, similarity_func_name="jaccard", similarity_threshold=0.5):
     """Find all pairs of sets with similarity greater than a threshold.
     This is an implementation of the All-Pair-Binary algorithm in the paper
     "Scaling Up All Pairs Similarity Search" by Bayardo et al., with
@@ -28,41 +32,54 @@ def all_pairs(sets, similarity_func_name="jaccard",
     if not isinstance(sets, list) or len(sets) == 0:
         raise ValueError("Input parameter sets must be a non-empty list.")
     if similarity_func_name not in _similarity_funcs:
-        raise ValueError("Similarity function {} is not supported.".format(
-            similarity_func_name))
+        raise ValueError(
+            "Similarity function {} is not supported.".format(similarity_func_name)
+        )
     if similarity_threshold < 0 or similarity_threshold > 1.0:
         raise ValueError("Similarity threshold must be in the range [0, 1].")
     if similarity_func_name not in _symmetric_similarity_funcs:
-        raise ValueError("The similarity function must be symmetric "
-        "({})".format(", ".join(_symmetric_similarity_funcs)))
+        raise ValueError(
+            "The similarity function must be symmetric "
+            "({})".format(", ".join(_symmetric_similarity_funcs))
+        )
     similarity_func = _similarity_funcs[similarity_func_name]
     overlap_threshold_func = _overlap_threshold_funcs[similarity_func_name]
     position_filter_func = _position_filter_funcs[similarity_func_name]
     sets, _ = _frequency_order_transform(sets)
     index = defaultdict(list)
-    logging.debug("Find all pairs with similarities >= {}...".format(
-        similarity_threshold))
+    logging.debug(
+        "Find all pairs with similarities >= {}...".format(similarity_threshold)
+    )
     count = 0
     for x1 in np.argsort([len(s) for s in sets]):
         s1 = sets[x1]
+        l1 = len(s1)
         t = overlap_threshold_func(len(s1), similarity_threshold)
         prefix_size = len(s1) - t + 1
         prefix = s1[:prefix_size]
         # Find candidates using tokens in the prefix.
-        candidates = set([x2 for p1, token in enumerate(prefix)
+        candidates = set(
+            [
+                x2
+                for p1, token in enumerate(prefix)
                 for x2, p2 in index[token]
-                if position_filter_func(s1, sets[x2], p1, p2,
-                    similarity_threshold)])
+                if position_filter_func(l1, len(sets[x2]), p1, p2, similarity_threshold)
+            ]
+        )
         for x2 in candidates:
             s2 = sets[x2]
-            sim = similarity_func(s1, s2)
+            sim = similarity_func(s1, s2, l1, len(s2))
             if sim < similarity_threshold:
                 continue
             # Output reverse-ordered set index pair (larger first).
-            yield tuple(sorted([x1, x2], reverse=True) + [sim,])
+            yield tuple(
+                sorted([x1, x2], reverse=True)
+                + [
+                    sim,
+                ]
+            )
             count += 1
         # Insert this prefix into index.
         for j, token in enumerate(prefix):
             index[token].append((x1, j))
     logging.debug("{} pairs found.".format(count))
-
--- scripts/all_pairs.py
+++ scripts/all_pairs.py
@@ -6,18 +6,20 @@ from collections import defaultdict, deque
 import csv
 import time
 import random
+
 random.seed(42)
 import numpy as np
 
 from SetSimilaritySearch import all_pairs, SearchIndex
 from SetSimilaritySearch.utils import _similarity_funcs
 
+
 def read_sets(set_filename, reversed=False, sample_k=0):
-    logging.info("Reading set tuples from {} (reversed={})...".format(
-        set_filename, reversed))
+    logging.info(
+        "Reading set tuples from {} (reversed={})...".format(set_filename, reversed)
+    )
     with open(set_filename) as f:
-        tuples = [line.strip().split() for line in f
-                if not line.startswith("#")]
+        tuples = [line.strip().split() for line in f if not line.startswith("#")]
     # Make sure the tuples are (SetID, Token).
     if reversed:
         tuples = [(y, x) for x, y in tuples]
@@ -36,35 +38,45 @@ def read_sets(set_filename, reversed=False, sample_k=0):
     logging.info("{} sets created.".format(len(sets)))
     return set_IDs, sets
 
+
 def self_all_pairs(set_IDs, sets, similarity_func_name, similarity_threshold):
     count = 0
-    logging.info("Find pairs with similarity >= {}.".format(
-        similarity_threshold))
-    for x, y, sim in all_pairs(sets,
-            similarity_func_name=similarity_func_name,
-            similarity_threshold=similarity_threshold):
+    logging.info("Find pairs with similarity >= {}.".format(similarity_threshold))
+    for x, y, sim in all_pairs(
+        sets,
+        similarity_func_name=similarity_func_name,
+        similarity_threshold=similarity_threshold,
+    ):
         yield (set_IDs[x], set_IDs[y], len(sets[x]), len(sets[y]), sim)
         count += 1
     logging.info("Found {} pairs.".format(count))
 
-def cross_collection_all_pairs(indexed_set_IDs, query_set_IDs, indexed_sets,
-        query_sets, similarity_func_name, similarity_threshold):
+
+def cross_collection_all_pairs(
+    indexed_set_IDs,
+    query_set_IDs,
+    indexed_sets,
+    query_sets,
+    similarity_func_name,
+    similarity_threshold,
+):
     logging.info("Building search index on {} sets.".format(len(indexed_sets)))
-    index = SearchIndex(indexed_sets, similarity_func_name=similarity_func_name,
-            similarity_threshold=similarity_threshold)
-    tokens = (token for s in query_sets for token in s if token not in index.order)
-    index.order.update((token, -i) for i, token in enumerate(tokens, 1))
+    index = SearchIndex(
+        indexed_sets,
+        similarity_func_name=similarity_func_name,
+        similarity_threshold=similarity_threshold,
+    )
     logging.info("Finished building search index.")
-    logging.info("Find pairs with similarity >= {}.".format(
-        similarity_threshold))
+    logging.info("Find pairs with similarity >= {}.".format(similarity_threshold))
     count = 0
     query_times = deque([])
     for set_ID, s in zip(query_set_IDs, query_sets):
         start = time.time()
         results = []
         for i, sim in index.query(s):
-            results.append((set_ID, indexed_set_IDs[i], len(s),
-                len(indexed_sets[i]), sim))
+            results.append(
+                (set_ID, indexed_set_IDs[i], len(s), len(indexed_sets[i]), sim)
+            )
             count += 1
         query_time = time.time() - start
         query_times.append(query_time)
@@ -75,44 +87,71 @@ def cross_collection_all_pairs(indexed_set_IDs, query_set_IDs, indexed_sets,
     query_times = np.array(list(query_times))
     logging.info("Average query time: {}.".format(np.mean(query_times)))
     logging.info("Median query time: {}.".format(np.mean(query_times)))
-    logging.info("90pct query time: {}.".format(
-        np.percentile(query_times, 90)))
+    logging.info("90pct query time: {}.".format(np.percentile(query_times, 90)))
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Find all pairs of sets with "
-        "similarities over a given threshold.")
-    parser.add_argument("--input-sets", required=True, nargs="+",
-            help="Input flattened set files with each line a (SetID Token) "
-            "tuple: 1 file for finding self all pairs (self-join); 2 files for "
-            "finding cross-collection all pairs (join).")
-    parser.add_argument("--output-pairs", required=True,
-            help="Output file with each line a "
-            "(SetID_X, SetID_Y, Size_X, Size_Y, Similarity) tuple.")
-    parser.add_argument("--similarity-func", choices=_similarity_funcs.keys(),
-        default="jaccard")
+    parser = argparse.ArgumentParser(
+        description="Find all pairs of sets with "
+        "similarities over a given threshold."
+    )
+    parser.add_argument(
+        "--input-sets",
+        required=True,
+        nargs="+",
+        help="Input flattened set files with each line a (SetID Token) "
+        "tuple: 1 file for finding self all pairs (self-join); 2 files for "
+        "finding cross-collection all pairs (join).",
+    )
+    parser.add_argument(
+        "--output-pairs",
+        required=True,
+        help="Output file with each line a "
+        "(SetID_X, SetID_Y, Size_X, Size_Y, Similarity) tuple.",
+    )
+    parser.add_argument(
+        "--similarity-func", choices=_similarity_funcs.keys(), default="jaccard"
+    )
     parser.add_argument("--similarity-threshold", type=float, default=0.5)
-    parser.add_argument("--reversed-tuple", type=bool, default=False,
-            help="Whether the input tuples are reversed i.e. (Token SetID).")
-    parser.add_argument("--sample-k", type=int, default=0,
-            help="The number of sampled sets from the second file to use as "
-            "queries; default use all sets.")
+    parser.add_argument(
+        "--reversed-tuple",
+        type=bool,
+        default=False,
+        help="Whether the input tuples are reversed i.e. (Token SetID).",
+    )
+    parser.add_argument(
+        "--sample-k",
+        type=int,
+        default=0,
+        help="The number of sampled sets from the second file to use as "
+        "queries; default use all sets.",
+    )
     args = parser.parse_args(sys.argv[1:])
     logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(message)s")
 
     if len(args.input_sets) == 1:
         set_IDs, sets = read_sets(args.input_sets[0], args.reversed_tuple)
-        pairs = self_all_pairs(set_IDs, sets, args.similarity_func,
-                args.similarity_threshold)
+        pairs = self_all_pairs(
+            set_IDs, sets, args.similarity_func, args.similarity_threshold
+        )
     elif len(args.input_sets) == 2:
         set_IDs_x, sets_x = read_sets(args.input_sets[0], args.reversed_tuple)
-        set_IDs_y, sets_y = read_sets(args.input_sets[1], args.reversed_tuple,
-                sample_k=args.sample_k)
-        pairs = cross_collection_all_pairs(set_IDs_x, set_IDs_y, sets_x,
-                sets_y, args.similarity_func, args.similarity_threshold)
+        set_IDs_y, sets_y = read_sets(
+            args.input_sets[1], args.reversed_tuple, sample_k=args.sample_k
+        )
+        pairs = cross_collection_all_pairs(
+            set_IDs_x,
+            set_IDs_y,
+            sets_x,
+            sets_y,
+            args.similarity_func,
+            args.similarity_threshold,
+        )
     else:
         raise ValueError("Number of input set files must be 1 or 2.")
     with open(args.output_pairs, "w") as f:
         writer = csv.writer(f)
-        writer.writerow(["set_ID_x", "set_ID_y", "set_size_x", "set_size_y",
-            "similarity"])
+        writer.writerow(
+            ["set_ID_x", "set_ID_y", "set_size_x", "set_size_y", "similarity"]
+        )
         writer.writerows(pairs)
