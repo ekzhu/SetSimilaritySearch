efed8d5da0ba76e05c07e17cd4801a4ba48d115d
Nolan Woods <nolan_w@sfu.ca>
2022-08-02 11:51:19 -0700

    Add test for #13

--- tests/search_test.py
+++ tests/search_test.py
@@ -13,6 +13,14 @@ class TestSearchIndex(unittest.TestCase):
         correct_results = set([(1, 1.0), (0, 0.2), (2, 0.5), (3, 0.2)])
         self.assertEqual(set(results), correct_results)
 
+    def test_query_unique(self):
+        sets = [[1,2,3], [3,4,5]]
+        index = SearchIndex(sets, similarity_func_name="jaccard",
+                similarity_threshold=0.1)
+        results = index.query([3,4,8])
+        correct_results = set([(0, 1/5), (1, 2/4)])
+        self.assertEqual(set(results), correct_results)
+
     def test_containment(self):
         sets = [[1, 2, 3], [3, 4, 5], [2, 3, 4], [5, 6, 7]]
         # Threshold 0.1
