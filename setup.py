75f866675ab864f12d10360fbe03077f86c8ff49
Eric Zhu <ekzhu@users.noreply.github.com>
2022-08-30 19:18:59 +0000

    update version number

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 from os import path
+
 # io.open is needed for projects that support Python 2.7
 # It ensures open() defaults to text mode with universal newlines,
 # and accepts an argument to specify the text encoding
@@ -9,7 +10,7 @@ from io import open
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
@@ -27,21 +28,18 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='SetSimilaritySearch',  # Required
-
+    name="SetSimilaritySearch",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.7',  # Required
-
+    version="1.0.0",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A Python library of set similarity search algorithms',  # Optional
-
+    description="A Python library of set similarity search algorithms",  # Optional
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
@@ -51,7 +49,6 @@ setup(
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
     long_description=long_description,  # Optional
-
     # Denotes that our long_description is in Markdown; valid values are
     # text/plain, text/x-rst, and text/markdown
     #
@@ -62,22 +59,18 @@ setup(
     #
     # This field corresponds to the "Description-Content-Type" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
+    long_description_content_type="text/markdown",  # Optional (see note above)
     # This should be a valid link to your project's main homepage.
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/ekzhu/SetSimilaritySearch',  # Optional
-
+    url="https://github.com/ekzhu/SetSimilaritySearch",  # Optional
     # This should be your name or the name of the organization which owns the
     # project.
-    author='Eric Zhu',  # Optional
-
+    author="Eric Zhu",  # Optional
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='ekzhu@cs.toronto.edu',  # Optional
-
+    author_email="ekzhu@cs.toronto.edu",  # Optional
     # Classifiers help users find your project by categorizing it.
     #
     # For a list of valid classifiers, see https://pypi.org/classifiers/
@@ -86,30 +79,25 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
+        "Development Status :: 3 - Alpha",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-
+        "Intended Audience :: Developers",
         # Pick your license as you wish
-        #"License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)",
-
+        # "License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-
     # This field adds keywords for your project which will appear on the
     # project page. What does your project relate to?
     #
     # Note that this is a string of words separated by whitespace, not a list.
-    keywords='set similarity search all pairs',  # Optional
-
+    keywords="set similarity search all pairs",  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -120,16 +108,14 @@ setup(
     #   py_modules=["my_module"],
     #
     # packages=find_packages(exclude=['contrib', 'docs', 'test*']),  # Required
-    packages=['SetSimilaritySearch'],
-
+    packages=["SetSimilaritySearch"],
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy'],  # Optional
-
+    install_requires=["numpy"],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
@@ -140,9 +126,8 @@ setup(
     # projects.
     extras_require={  # Optional
         #'dev': ['check-manifest'],
-        'test': ['coverage', 'nose'],
+        "test": ["coverage", "nose"],
     },
-
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     #
@@ -151,14 +136,12 @@ setup(
     # package_data={  # Optional
     #     'sample': ['package_data.dat'],
     # },
-
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     #
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],  # Optional
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
@@ -171,11 +154,9 @@ setup(
     #         'sample=sample:main',
     #     ],
     # },
-
-    scripts = [
-        'scripts/all_pairs.py',
+    scripts=[
+        "scripts/all_pairs.py",
     ]
-
     # List additional URLs that are relevant to your project as a dict.
     #
     # This field corresponds to the "Project-URL" metadata fields:
@@ -192,4 +173,3 @@ setup(
     #     'Source': 'https://github.com/pypa/sampleproject/',
     # },
 )
-
